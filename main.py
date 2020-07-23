76f8034d6994742be042ba5ca82905cc534c0ccb
Kayla Lemessy <kaylalemessy15@gmail.com>
2020-07-23 14:30:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#  They are endemic to the wilds of Australia.
+#The average size is 18-22 in, while the average weight is 10-18 oz.
+#Today, there are 8 recogninzed species.
+#As semi-arboreal creatures,they sometimes perch on tree branches and fence posts.
+i = int(input( "Give me a number"))
+if  a== 1:
+  print(" They are endemic to the wilds of Australia.")
+
+elif b== 2:
+  print("The average size is 18 -22 in, while the average weight is.")
+
+elif c==3:
+  print("Today , ther are 8 recongnized species. ")
+
+else d==4:
+ print (" As semi arboreal creatures they sometimes perch on tree branches and fence posts.")
+
+
